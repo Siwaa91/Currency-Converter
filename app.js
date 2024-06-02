cc16d8854cdfa4ce9e3f886de1511336b39647fe
Siwaa91 <shivakb910@gmail.com>
2024-06-02 21:17:15 +0530

    updated

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ const BASE_URL =
       let newOption = document.createElement("option");
       newOption.innerText = currCode;
       newOption.value = currCode;
+      if(select.name === "from" && currCode === "USD"){
+        newOption.selected = "selected";
+      } else if(select.name === "to" && currCode === "INR"){
+        newOption.selected = "selected";
+      }
       select.append(newOption);
     }
+
+    select.addEventListener("change", (evt) => {
+      updateFlag(evt.target);
+    });
+  }
+
+  const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
   }
